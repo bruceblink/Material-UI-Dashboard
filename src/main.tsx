c88d457c783dfce19f929baa3809807491c3dfda
likanug <likanug.g@qq.com>
2025-07-22 09:33:07 +0800

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import Dashboard from './dashboard/Dashboard.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>,
+);
